7dd01fe29979d44d3528ee9c6e7f0867c300db1a
Dev-43 <deveshdolas9@gmail.com>
2026-09-05 18:43:25 +0530

    Fix: Remove unneeded tracked database files (.pgdata) and powershell.cmd, update .gitignore

--- server/powershell.cmd
+++
@@ -1,3 +0,0 @@
-@echo off
-set "PATH=C:\Windows\System32;C:\Windows\System32\WindowsPowerShell\v1.0;C:\Program Files\nodejs;C:\Users\GAJANAN\AppData\Roaming\npm;C:\Program Files\Git\cmd;%PATH%"
-powershell.exe -NoProfile -ExecutionPolicy Bypass %*
